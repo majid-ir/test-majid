9b14f7b269f2c88fd4616a93ee7f30cf9a309589
mj <majid.pnu86@gmail.com>
2018-10-04 23:52:27 +0330

    models complete

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Expenses,Incoms
 
-# Register your models here.
+admin.site.register(Expenses)
+admin.site.register(Incoms)
--- web/models.py
+++ web/models.py
@@ -2,14 +2,28 @@ from django.db import models
 
 from django.contrib.auth.models import User
 
-class Expands(models.Model):
+class Expenses(models.Model):
 
     text=models.CharField(max_length=255)
     date=models.DateTimeField('date')
     amount=models.BigIntegerField()
-    user=models.ForeignKey(User)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        mydate=self.date.strftime("%Y-%m-%d")
+        return ("{} - {}".format(mydate,self.amount))
 
 
+class Incoms(models.Model):
+
+    text=models.CharField(max_length=255)
+    date=models.DateTimeField('dates')
+    amount=models.BigIntegerField()
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        mydate=self.date.strftime("%Y-%m-%d")
+        return ("{} - {}".format(mydate,self.amount))
 
 
 
