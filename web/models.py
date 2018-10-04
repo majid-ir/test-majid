40c82fc4d44090207c7462cb5199263c2690f53b
mj <majid.pnu86@gmail.com>
2018-10-04 19:44:46 +0330

    add expense models

--- web/models.py
+++ web/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class Expands(models.Model):
+
+    text=models.CharField(max_length=255)
+    date=models.DateTimeField('date')
+    amount=models.BigIntegerField()
+    user=models.ForeignKey(User)
+
+
+
+
+
